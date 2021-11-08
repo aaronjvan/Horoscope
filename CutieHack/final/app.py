7c1dc5d6d195047e8f51a7f307e94cd2e19cd978
Hgxegz1 <hgxegz21@gmail.com>
2021-11-07 21:10:55 -0800

    final

---
+++ CutieHack/final/app.py
@@ -0,0 +1,29 @@
+from flask import Flask, request
+from flask_cors import CORS
+from main import *
+
+app = Flask(__name__)
+
+CORS(app)
+@app.route('/')
+def index():
+	return "Hello World!"
+
+@app.route('/test')
+def test():
+	return "Hello test!"
+
+#@app.route('getHoroscope', methods=['GET'])
+#def horoscope('/horoscope')
+
+@app.route('/horoscope', methods=['GET'])
+def horoscope():
+    birthMonth = request.args.get('m')
+    birthDay = request.args.get('d')
+    return returnHoroscope(birthMonth, birthDay)
+    
+@app.route('/playlist', methods=['GET'])
+def songLink():
+    birthMonth = request.args.get('m')
+    birthDay = request.args.get('d')
+    return returnLink(birthMonth, birthDay)
---
+++ CutieHack/final/main.py
@@ -0,0 +1,40 @@
+from pyhoroscope import *
+from emotionAnalysis import *
+from secrets import *
+from spotPlaylistGetter import *
+
+moodPlaylist = {"happy": "79QRwz97ZME65XdEp1eqQm?si=4791f0586e104ae5",
+                 "sad": "5nT9FdSNMNTwNkvdX5lkta?si=6638465833bf4bcd", 
+                 "loved": "1XPefR0TaISwZXw8ltYew4?si=c95f6aa7e2c04548",
+                 "angry": "5NAiz92PO6eD61j5lGhrhY?si=07ecbd3dd2744f5c",
+                 "scared": "1r4hnyOWexSvylLokn2hUa?si=62928bc1d0ce4756"}
+
+def returnHoroscope(birthMonth, birthDay):
+    horo = Horoscope()
+    sign = getStarSign(str(birthMonth), int(birthDay))
+    data = horo.get_todays_horoscope(sign)
+    dailyMessage = data['horoscope']
+    return dailyMessage
+
+def returnLink(birthMonth, birthDay):
+    horo = Horoscope()
+    sign = getStarSign(str(birthMonth), int(birthDay))
+    data = horo.get_todays_horoscope(sign)
+    dailyMessage = data['horoscope']
+    print(dailyMessage)
+    mood = analyzeText(dailyMessage, 'emotions.txt')
+    #THIS IS THE WHOLE API REQUEST
+    
+    token = getAccessToken(ClientID, ClientSecret)
+    if(mood == " happy"):
+        playlistID = (moodPlaylist.get("happy"))
+    elif(mood == " sad"):
+        playlistID = (moodPlaylist.get("sad"))
+    elif(mood == " angry"):
+        playlistID = (moodPlaylist.get("angry"))
+    elif(mood == " scared"):
+        playlistID = (moodPlaylist.get("scared"))
+    else:
+        playlistID = (moodPlaylist.get("loved"))
+    
+    return "https://open.spotify.com/embed/playlist/"+playlistID
